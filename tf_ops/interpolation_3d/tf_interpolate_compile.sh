58c5a99325df77059b67a10b06d5c3504c319448
ltriess <mail@triess.eu>
2020-05-14 09:48:50 +0200

    Update compile scripts

--- tf_ops/interpolation_3d/tf_interpolate_compile.sh
+++ tf_ops/interpolation_3d/tf_interpolate_compile.sh
@@ -1,5 +1,10 @@
-# TF1.2
-g++ -std=c++11 tf_interpolate.cpp -o tf_interpolate_so.so -shared -fPIC -I /usr/local/lib/python2.7/dist-packages/tensorflow/include -I /usr/local/cuda-8.0/include -lcudart -L /usr/local/cuda-8.0/lib64/ -O2 -D_GLIBCXX_USE_CXX11_ABI=0
+#!/bin/bash
+# tested with TF1.15 and cuda-10.0 (/usr/local/cuda --> /usr/local/cuda-10.0)
+# make sure to source the correct python virtualenv first, otherwise no or the wrong tensorflow will be found
+TF_CFLAGS=( $(python -c 'import tensorflow as tf; print(" ".join(tf.sysconfig.get_compile_flags()))') )
+TF_LFLAGS=( $(python -c 'import tensorflow as tf; print(" ".join(tf.sysconfig.get_link_flags()))') )
 
-# TF1.4
-#g++ -std=c++11 tf_interpolate.cpp -o tf_interpolate_so.so -shared -fPIC -I /usr/local/lib/python2.7/dist-packages/tensorflow/include -I /usr/local/cuda-8.0/include -I /usr/local/lib/python2.7/dist-packages/tensorflow/include/external/nsync/public -lcudart -L /usr/local/cuda-8.0/lib64/ -L/usr/local/lib/python2.7/dist-packages/tensorflow -ltensorflow_framework -O2 -D_GLIBCXX_USE_CXX11_ABI=0
+# TF_CFLAGS: -I/path/to/venv/lib/python3.6/site-packages/tensorflow_core/include -D_GLIBCXX_USE_CXX11_ABI=0
+# TF_LFLAGS: -L/path/to/venv/lib/python3.6/site-packages/tensorflow_core -l:libtensorflow_framework.so.1
+
+g++ -std=c++11 -shared tf_interpolate.cpp -o tf_interpolate_so.so ${TF_CFLAGS[@]} -fPIC -lcudart ${TF_LFLAGS[@]} -L/usr/local/cuda/lib64
